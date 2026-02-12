f2c82d505c2c7c6f6ab4f7ebeadeedb9c36ea862
Gabriel J. Csapo <gabecsapo@gmail.com>
2026-02-11 20:02:48 -0500

    feat: adds favicon

--- packages/web/app/root.tsx
+++ packages/web/app/root.tsx
@@ -9,6 +9,7 @@ export function Layout({ children }: { children: React.ReactNode }) {
       <head>
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
         <title>Groffee</title>
       </head>
       <body className="flex flex-col min-h-screen">
